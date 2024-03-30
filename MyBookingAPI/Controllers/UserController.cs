74d9b30e3d0247b89a34e90ca37032e5c2c3f7b6
colman-wen <colmanwen@163.com>
2024-03-29 23:57:10 -0700

    Create user sign in endpoint

--- MyBookingAPI/Controllers/UserController.cs
+++ MyBookingAPI/Controllers/UserController.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBookingAPI.Model;
 
 namespace MyBookingAPI.Controllers
 {
@@ -7,5 +7,32 @@ namespace MyBookingAPI.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        /// <summary>
+        /// Logger
+        /// </summary>
+        private readonly ILogger<UserController> _logger;
+
+        /// <summary>
+        /// Constructor with Logger
+        /// </summary>
+        /// <param name="logger">Logger</param>
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Endpoint for user to sign in
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns>If sign in is susseceful, returns a token</returns>
+        [HttpPost]
+        public SignInResponse SignIn(string userName, string password) {
+            return new SignInResponse
+            {
+                Token = new Guid().ToString()
+            };
+        }
     }
 }
---
+++ MyBookingAPI/Model/Response.cs
@@ -0,0 +1,10 @@
+﻿namespace MyBookingAPI.Model
+{
+    public abstract class Response
+    {
+        protected Response() { }
+        public string? Message {  get; set; }
+
+        public int Status { get; set; } = 0;
+    }
+}
