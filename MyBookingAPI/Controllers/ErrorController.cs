291ab35cbda0705348de2304dff2e2d9bfca0ea2
colman-wen <colmanwen@163.com>
2024-03-30 13:05:30 -0700

    Add Error controller

---
+++ MyBookingAPI/Controllers/ErrorController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MyBookingAPI.Model;
+
+namespace MyBookingAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var error = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            if (error != null)
+            {
+                var exception = error.Error;
+                return StatusCode(500, 
+                    new Response
+                    {
+                        Status = 500,
+                        Message = exception.Message
+                    }
+                );
+            }
+
+            return StatusCode(500,
+                new Response
+                {
+                    Status = 500,
+                    Message = "Internal Server Error"
+                }
+            );
+        }
+    }
+}
--- MyBookingAPI/Program.cs
+++ MyBookingAPI/Program.cs
@@ -14,6 +14,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/error");
 }
 
 app.UseHttpsRedirection();
