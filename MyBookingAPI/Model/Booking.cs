eb5744b3a8eba393f1f57d5c0dac686c4c96ca1b
colman-wen <colmanwen@163.com>
2024-04-01 10:56:05 -0700

    Update comments

--- MyBookingAPI/Model/Booking.cs
+++ MyBookingAPI/Model/Booking.cs
@@ -1,15 +1,33 @@
 ﻿namespace MyBookingAPI.Model
 {
+    /// <summary>
+    /// A model to record when a user book tickets for a event
+    /// </summary>
     public class Booking
     {
+        /// <summary>
+        /// The User ID who books the tickets
+        /// </summary>
         public string UserID { get; set; }
 
+        /// <summary>
+        /// A unique ID for this Booking. Value can be assigned after Booking has been placed against the event
+        /// </summary>
         public string? BookingID { get; set; }
 
+        /// <summary>
+        /// The ID of event to be booked for the user
+        /// </summary>
         public string EventID { get; set; }
 
-        public  Dictionary<EventType, int> Counts { get; set; }
+        /// <summary>
+        /// Counts the number of tickets to be booked with each Event Type
+        /// </summary>
+        public  Dictionary<TicketType, int> Counts { get; set; }
 
+        /// <summary>
+        /// Indicates whether a Payment has been made against this Booking
+        /// </summary>
         public bool Paid { get; set; }
     }
 }
--- MyBookingAPI/Model/User.cs
+++ MyBookingAPI/Model/User.cs
@@ -1,13 +1,28 @@
 ﻿namespace MyBookingAPI.Model
 {
+    /// <summary>
+    /// Model for a User
+    /// </summary>
     public class User
     {
+        /// <summary>
+        /// A unique identifier of a User
+        /// </summary>
         public string UserID { get; set; }
 
+        /// <summary>
+        /// Name of the User
+        /// </summary>
         public string UserName { get; set; }
 
+        /// <summary>
+        /// Email of the User
+        /// </summary>
         public string UserEmail { get; set; }
 
+        /// <summary>
+        /// Password of the User
+        /// </summary>
         public string Password { get; set; }
     }
 }
